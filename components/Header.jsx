f1d0ff2e29d8b823d39ccb30f13f4fd196a77edf
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-10-10 21:31:11 +0300

    Refactor Header component and add MobileNav

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import Nav from './Nav'
 import { Button } from './ui/button'
+import MobileNav from './MobileNav'
 
 function Header() {
     return (
@@ -20,7 +21,7 @@ function Header() {
                 </div>
                 {/* Mobile nav */}
                 <div className="xl:hidden">
-                    mobile nav
+                    <MobileNav />
                 </div>
             </div>
         </header>
