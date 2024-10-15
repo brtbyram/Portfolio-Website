ed1037eb91d7c7f955ad1b99169b15f0f0357879
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-10-15 18:41:57 +0300

    Refactor Home page layout, add Photo and Stats components

--- app/page.js
+++ app/page.js
@@ -7,12 +7,13 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 pb-12 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1 mb-6">
@@ -40,6 +41,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <Stats/>
     </section>
   );
 }
